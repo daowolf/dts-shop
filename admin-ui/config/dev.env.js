51888fc31c4153943b4eca4203044bac8fe20892
wenwu <wenwu07093@163.com>
2021-12-25 22:00:03 +0800

    get from gitee for study
    @wenwu get from gitee for study

---
+++ admin-ui/config/dev.env.js
@@ -0,0 +1,11 @@
+module.exports = {
+	NODE_ENV: '"development"',
+    ENV_CONFIG: '"dev"',
+    BASE_API: '"http://localhost:8083/demo/admin"'
+}
+
+/*module.exports = {
+    NODE_ENV: '"development"',
+    ENV_CONFIG: '"dev"',
+    BASE_API: '"http://localhost:8084/merchant"'
+}*/
